917219f80a559bd6bea05bc185bc13ef512fcf59
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-02-20 18:48:00 -0600

    Update main.py

--- interpreter/main.py
+++ interpreter/main.py
@@ -1,6 +1,5 @@
 from tokenizer import *
 from parser import *
-import sys
 with open(input("<usage> Enter .rh script (must be in current directory)\n"), 'r')as script:
 	for line in script.read():
 		parse(tokenize(line))
